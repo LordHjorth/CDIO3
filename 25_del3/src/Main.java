35b8903561e9758f43aca7f52c6e70d67b89cc7e
simonezethof <simone.zethof@hotmail.dk>
2017-11-13 16:12:17 +0100

    Mit rigtig nice program

--- 25_del3/src/Main.java
+++ 25_del3/src/Main.java
@@ -1,3 +1,4 @@
+
 import gui_main.GUI;
 
 public class Main {
@@ -8,7 +9,34 @@ public class Main {
 		
 		gui.showMessage("HEJ");
 		
+		gui.showMessage("SIMONE ER SUPER NICE");
+
+		if (gui.getUserLeftButtonPressed("Simone er nice", "NEJ", "JA")==true) {
+			gui.showMessage("Du lyver vidst");
+		}
+			else {
+				gui.showMessage("Du har helt styr på det");
+				
+		}
 		
-		
+		if (gui.getUserSelection(" Vælg den som er mest nice ", "Simone", "Rasmus ", "Nicholas", "Simon", "Emil") == "Simone") {
+			gui.showMessage("Du har helt styr på det");
+		}
+		 if (gui.getUserSelection(" Vælg den som er mest nice ", "Simone", "Rasmus ", "Nicholas", "Simon", "Emil") == "Rasmus") {
+			gui.showMessage("aaaaaaargh");
+		}
+		 if (gui.getUserSelection(" Vælg den som er mest nice ", "Simone", "Rasmus ", "Nicholas", "Simon", "Emil") == "Nicholas") {
+			gui.showMessage("Det kan du da ikke mene");
+		}
+		 if (gui.getUserSelection(" Vælg den som er mest nice ", "Simone", "Rasmus ", "Nicholas", "Simon", "Emil") == "Simon") {
+			gui.showMessage("Det må vidst være en joke");
+		}
+		 if (gui.getUserSelection(" Vælg den som er mest nice ", "Simone", "Rasmus ", "Nicholas", "Simon", "Emil") == "Emil") {
+			gui.showMessage("i en lille grad");
+		}
+
+
+
+
 	}
 }
