ed78002fdc5212a42c8442428226eda88c12aa2a
LordHjorth <hjorthen96@hotmail.com>
2017-11-27 17:36:18 +0100

    jhgjhg

---
+++ 25_del3/src/test/Konto.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Konto {
+
+	private int kapital;
+
+	public Konto(int kapital) {
+		this.setKapital(kapital);
+	}
+
+	public int getKapital() {
+		return kapital;
+	}
+
+	public void setKapital(int kapital) {
+		this.kapital = kapital;
+	}
+
+}
---
+++ 25_del3/src/test/SpilController.java
@@ -0,0 +1,49 @@
+package test;
+
+public class SpilController {
+
+	private final int STARTFELT = 0;
+	private final int START_KAPITAL = 31;
+	private GUIController guiController;
+	private SpillerController spillerController;
+	private BraetController braetController;
+	private NySpiller[] spillere;
+
+	public void playGame() {
+		this.guiController = new GUIController();
+		this.spillerController = new SpillerController();
+		this.braetController = new BraetController();
+		initializeGame();
+		gameloop();
+		
+		
+		
+	}
+
+	private void gameloop() {
+		boolean noWinner = false;
+		while (noWinner) {
+
+
+			
+		}
+		
+	}
+
+	private void initializeGame() {
+		// Find ud af antalSpillere
+		int antalSpillere = guiController.getAntalSpillere();
+		this.spillere = new NySpiller[antalSpillere];
+		
+		for (int i = 0; i < this.spillere.length; i++) {
+			String navn = guiController.getNavnPåSpiller();
+			this.spillere[i] = new NySpiller(navn, START_KAPITAL, STARTFELT);
+		}
+		guiController.addPlayers(spillere);
+		//Sæt brættet op;
+		
+		
+		
+	}
+
+}
