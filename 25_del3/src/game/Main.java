526fb72b25078db8b7005c49eeece68c218244f8
LordHjorth <hjorthen96@hotmail.com>
2017-12-12 14:43:40 +0100

    git test

--- 25_del3/src/game/Main.java
+++ 25_del3/src/game/Main.java
@@ -5,5 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		SpilController spilController = new SpilController(); //opretter et objekt af SpilController
 		spilController.playGame(); //på det objekt kaldes meotden PlayGame som indeholder opsætning af gui, spillet og gameloopet.
+		
+		
+		//hej - test
 	} 
 }
